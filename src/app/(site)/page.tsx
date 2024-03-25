f79cb472770ba9ff6f78c130fffae4981deb99ba
Sparsh23AK <sparshgiri23@gmail.com>
2024-03-25 17:12:12 +0530

    finishing up sign up page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -27,7 +27,7 @@ const HomePage = () => {
             variant="btn-primary"
             className="w-full rounded-[10px] p-6 text-2xl bg-background"
           >
-            Get Cypress Free
+            Get Planner Free
           </Button>
         </div>
         <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
