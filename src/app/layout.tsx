8e0174506839f3bb9a923dbc29a6e19b8f105de4
Sparsh23AK <sparshgiri23@gmail.com>
2024-03-29 13:47:31 +0530

    initializing state-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import db from "../lib/supabase/db";
 import { ThemeProvider } from "../lib/providers/next-theme-provider";
 import { DM_Sans } from "next/font/google";
 import { twMerge } from "tailwind-merge";
+import AppStateProvider from "../lib/providers/state-provider";
+import { Toaster } from "../components/ui/toaster";
 
 const inter = DM_Sans({ subsets: ["latin"] });
 
@@ -28,15 +30,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  console.log(db);
+}) {
   return (
     <html lang="en">
       <body className={twMerge('bg-background', inter.className)}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+        >
+          <AppStateProvider>
+                {children}
+                <Toaster />
+          </AppStateProvider>
         </ThemeProvider>
       </body>
     </html>
